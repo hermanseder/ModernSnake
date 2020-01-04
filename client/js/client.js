699da1a6071f847fed26fa20749bcd85c959875f
Rudolf Hermanseder <s1810307016@students.fh-hagenberg.at>
2020-01-04 22:10:52 +0100

    old code removed

--- client/js/client.js
+++ client/js/client.js
@@ -1,9 +1,7 @@
 /* Declarations */
-// let loginUiHandler;
-
 const socket = io();
 
-$(document).ready(function () {
+$(document).on('ready', function () {
     SocketHandler.initialize(socket);
     LoginHandler.initialize(socket);
 
@@ -14,59 +12,4 @@ $(document).ready(function () {
     LoginHandler.login('rudi', 'pw');
 
     socket.emit('test', 'client');
-});
-
-
-socket.on('authenticated', function() {
-    document.getElementById("loginState").innerHTML = true;
-});
-socket.on('unauthorized', function(err){
-    console.log("There was an error with the authentication:", err.message);
-});
-
-function login() {
-    console.log('login');
-    socket.emit('authentication', {username: "Rudi", password: "test123"});
-}
-
-socket.on('update', function(data) {
-    console.log(data);
-    document.getElementById("result").innerHTML = data;
-});
-
-var movement = {
-    up: false,
-    down: false,
-    left: false,
-    right: false
-}
-document.addEventListener('keyup', function(event) {
-    let mov;
-    switch (event.keyCode) {
-        case 65: // A
-            mov = 'A';
-            break;
-        case 87: // W
-            mov = 'W';
-            break;
-        case 68: // D
-            mov = 'D';
-            break;
-        case 83: // S
-            mov = 'S';
-            break;
-        case 37:  //ARROW LEFT
-            mov = 'LEFT';
-            break; 
-        case 38: //ARROW UP
-            mov = 'UP';
-            break; 
-        case 39: //ARROW RIGHT
-            mov = 'RIGHT'; 
-            break; 
-        case 40: //ARROW DOWN
-            mov = 'DOWN'; 
-            break; 
-    }
-    socket.emit('movement', mov);
 });
